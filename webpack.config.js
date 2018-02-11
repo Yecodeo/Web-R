039f3ee9b89c5a2b38389a82be31481e194aa3e4
yecodeo <yecodeo@gmail.com>
2018-02-11 05:56:43 +0100

    adding babel, eslint plugin, webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -33,62 +33,36 @@ var config = {
         use: {
             loader: 'babel-loader',
             options: {
-                presets: ['react', 'env']
+                presets: ['react', 'env'],
+                plugins: ['transform-class-properties']
             }
         }
-    },
-
+      },
+      {
+				test: /\.sass$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["style-loader", "css-loader","sass-loader" ] // compiles Sass to CSS
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      },
     ],
     loaders: [
-      // Transform all ES6 files to plain old ES5.
-      			{
-      				test: /\.(js|jsx)$/,
-      				exclude: [/bower_components/, /node_modules/, /styles/],
-      				loader: 'babel-loader',
-      			},
-      			// Load images.
-      			{
-      				test: /\.(gif|jpe?g|png)$/,
-      				loader: 'url-loader?limit=25000',
-      				query: {
-      					limit: 10000,
-      					name: 'static/media/images/[name].[hash:8].[ext]'
-      				}
-      			},
-      			{
-      				test: /\.scss$/,
-      				loaders: ['style-loader', 'css-loader', 'sass-loader', 'resolve-url-loader?sourceMap', 'sass-loader?sourceMap'],
-      				include: path.resolve(__dirname, '../../')
-      			},
-      			{
-      				test: /\.css$/,
-      				loader: 'style!css?importLoaders=1'
-      			},
-      			// Fonts
-      			{
-      				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      				loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      				query: {
-      					name: 'static/media/files/[name].[hash:8].[ext]'
-      				}
-      			},
-      			{
-      				test: /\.svg$/,
-      				use: [
-      					{
-      						loader: 'babel-loader',
-      						query: {
-      							presets: ['airbnb'],
-      						},
-      					},
-      					{
-      						loader: 'react-svg-loader',
-      						query: {
-      							jsx: true,
-      						},
-      					},
-      				],
-      			},
+			{
+				test: /\.sass$/,
+				loaders: ['style-loader', 'css-loader', 'sass-loader', 'resolve-url-loader?sourceMap', 'sass-loader?sourceMap'],
+				include: path.resolve(__dirname, '../../')
+			},
+			{
+				test: /\.css$/,
+				loader: 'style!css?importLoaders=1'
+			}
     ]
   },
  plugins: [HtmlWebpackPluginConfig]
